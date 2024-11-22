8951f68bbafb35a6b6a82df6260a97b1993cbfba
Daniil Zotov <ddazo002@edu.xamk.fi>
2024-11-22 05:09:59 +0200

    improves code readability, adds missed descriptions

--- src/components/screens/card-screen.tsx
+++ src/components/screens/card-screen.tsx
@@ -1,31 +1,15 @@
 import { useState, useEffect } from "react";
-import {
-  Avatar,
-  AvatarGroup,
-  Badge,
-  Box,
-  Button,
-  Card,
-  CardContent,
-  CardActions,
-  CircularProgress,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  IconButton,
-  TextField,
-  Tooltip,
-  Typography
-} from "@mui/material";
+import {Box} from "@mui/material";
 import { useLambdasApi } from "src/hooks/use-api";
 import { TrelloCard, TrelloMember } from "src/generated/homeLambdasClient";
-import ReactMarkdown from "react-markdown";
-import UserRoleUtils from "src/utils/user-role-utils";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline"
 import strings from "src/localization/strings";
 import { useSetAtom } from "jotai";
 import { errorAtom } from "src/atoms/error";
+import CardForm from "../trello-cards/card-form-component";
+import CardFilter from "../trello-cards/card-filter";
+import CardList from "../trello-cards/card-list";
+import ImagePreviewDialog from "../trello-cards/image-preview-dialog";
+import CardDialog from "../trello-cards/card-dialog";
 
 /**
  * Card screen component
@@ -44,7 +28,6 @@ const CardScreen = () => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
   const { trelloApi } = useLambdasApi();
-  const adminMode = UserRoleUtils.adminMode();
   const setError = useSetAtom(errorAtom);
 
   useEffect(() => {
@@ -196,319 +179,42 @@ const CardScreen = () => {
     setSelectedImage(null);
     setIsImageOpen(false);
   };
-
-  /**
-   * Renders an avatar for a specific member
-   * 
-   * @param memberId member ID
-   */
-  const AvatarIcon = ({memberId}:{memberId: string}) => {
-    const assignedMember = members.find((m) => m.memberId === memberId);
-  
-    if (!assignedMember) {
-      return null;
-    }
-    const { fullName } = assignedMember;
-    const initials = fullName?.split(" ").map((word) => word[0]).join("").toUpperCase();
-  
-    return (
-      <Tooltip title={fullName || `${strings.cardRequestError.unknownMember}`}>
-        <Avatar
-          sx={{
-            bgcolor: "primary.main",
-            color: "white",
-            width: 35,
-            height: 35,
-            fontSize: "1rem",
-          }}
-        >
-          {initials}
-        </Avatar>
-      </Tooltip>
-    );
-  };
-
-  /**
-   * Renders a grouped avatars for assigned members
-   * 
-   * @param memberIds The list of member IDs to display
-   */
-  const AvatarWrapper = ({ memberIds }: { memberIds: string[] }) => {
-    const maxAvatars = 3;
-    const hiddenMembers = memberIds.slice(maxAvatars - 1);
-
-    return (
-      <AvatarGroup max={maxAvatars} sx={{ justifyContent: "flex-end" }}>
-        {memberIds.slice(0, maxAvatars - 1).map((id) => (
-          <AvatarIcon key={id} memberId={id} />
-        ))}
-      {hiddenMembers.length > 0 && (
-        <Tooltip
-          title={
-            <Box>
-              {hiddenMembers
-                .map(
-                  (id) =>
-                    members.find((member) => member.memberId === id)?.fullName || `${strings.cardRequestError.unknownMember}`
-                )
-                .map((name, index) => (
-                  <div key={index}>
-                    {name}
-                  </div>
-                ))}
-          </Box>
-          }
-        >
-          <Avatar
-            sx={{
-              bgcolor: "secondary.main",
-              color: "white",
-              width: 35,
-              height: 35,
-              fontSize: "0.8rem",
-            }}
-          >
-            +{memberIds.length - (maxAvatars - 1)}
-          </Avatar>
-        </Tooltip>
-      )}
-      </AvatarGroup>
-    );
-  };
   
   return (
     <Box>
-      <Card sx={{ mb: 2, p: 2 }}>
-        <Typography variant="h6">{strings.cardScreen.createNewCard}</Typography>
-        <TextField
-          label={strings.cardScreen.title}
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-          fullWidth
-          sx={{ mt: 1 }}
-        />
-        <TextField
-          label={strings.cardScreen.description}
-          value={description}
-          onChange={(event) => setDescription(event.target.value)}
-          multiline
-          rows={4}
-          fullWidth
-          sx={{ mt: 1 }}
-        />
-        <Button onClick={createCard} variant="contained" sx={{ mt: 1 }}>
-          {strings.cardScreen.createCard}
-        </Button>
-      </Card>
-      <Card sx={{ mb: 2, p: 2 }}>
-        <TextField
-          label={strings.cardScreen.filterCards}
-          value={filterQuery}
-          onChange={handleFilterChange}
-          fullWidth
-        />
-      </Card>
-      <Box
-        sx={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(350px, 1fr))",
-          gap: 2,
-        }}
-      >
-        {loading ? (
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100vh",
-              position: "fixed",
-              top: 0,
-              left: 0,
-              width: "100%"
-            }}
-          >
-            <CircularProgress />
-          </Box>
-        ) : (
-        <>
-          {filteredCards.map((card: any) => (
-            <Card key={card.cardId} sx={{ 
-              display: "flex", 
-              flexDirection: "column", 
-              height: "400px",
-              position: "relative" 
-            }}>
-              <CardContent sx={{ flexGrow: 1, height: '40%' }}>
-                <Typography variant="h6">{card.title}</Typography>
-                <Box style={{height: '80%', overflow: 'hidden'}}>
-                  <ReactMarkdown
-                    components={{
-                      img: ({ node, ...props }) => (
-                        <img {...props} style={{ width: "100%", height: "auto" }} alt={props.alt || "Image"} />
-                      ),
-                    }}
-                  >
-                    {card.description ? card.description : `${strings.cardRequestError.noDescription}`}
-                  </ReactMarkdown>
-                </Box>
-              </CardContent>
-              <CardActions sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                padding: "8px 16px"
-              }}>
-                <Box sx={{ display: "flex", gap: "8px" }}>
-                  <Button onClick={() => openCard(card)} variant="outlined" sx={{ position: "relative", bottom: 0 }}>
-                    {strings.cardScreen.open}
-                  </Button>
-                  {!adminMode && (
-                    <Button onClick={() => deleteCard(card.cardId)} variant="outlined" color="error">
-                      {strings.cardScreen.delete}
-                    </Button>
-                  )}
-                </Box>
-                <Box
-                  sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "8px",
-                }}
-                >
-                  <Tooltip title={card.comments.length > 0 ?
-                    <Box>
-                      {card.comments?.reduce((sum: any, comment: any) => {
-                        const author = members.find(member => member.memberId === comment.createdBy)?.fullName || '';
-                        const existing = sum.find((item: any) => item.includes(author));
-                        if (existing) {
-                          const count = parseInt(existing.split(" ")[0]) + 1;
-                          sum = sum.filter((item: any) => !item.includes(author));
-                          sum.push(
-                            count > 1
-                              ? strings.cardScreen.messagesFrom
-                                .replace("{count}", count.toString())
-                                .replace("{author}", author)
-                              : strings.cardScreen.messageFrom
-                                .replace("{count}", "1")
-                                .replace("{author}", author)
-                          );
-                        } else {
-                          sum.push(
-                            strings.cardScreen.messageFrom
-                              .replace("{count}", "1")
-                              .replace("{author}", author)
-                          );
-                        }
-                        return sum;
-                      }, [])
-                        .map((text: any, index: any) => (
-                          <div key={index}>{text}</div>
-                        )) || <div>{strings.cardRequestError.noComments}</div>}
-                    </Box>
-                    : ""
-                  }>
-                    <IconButton>
-                      <Badge
-                        badgeContent={card.comments?.length || 0}
-                        color="primary"
-                        overlap="circular"
-                      >
-                        <ChatBubbleOutlineIcon />
-                      </Badge>
-                    </IconButton> 
-                  </Tooltip>
-                </Box>
-                <AvatarWrapper memberIds={card.assignedPersons} />
-              </CardActions>
-            </Card>
-          ))}
-        </>
-      )}
-      </Box>
+      <CardForm
+        title={title}
+        description={description}
+        onTitleChange={(event) => setTitle(event.target.value)}
+        onDescriptionChange={(event) => setDescription(event.target.value)}
+        onCreateCard={createCard}
+      />
+      <CardFilter filterQuery={filterQuery} onFilterChange={handleFilterChange} />
+      <CardList
+        cards={filteredCards}
+        loading={loading}
+        onOpenCard={openCard}
+        onDeleteCard={deleteCard}
+        members={members}
+      />
       <Box>
         {selectedCard && (
-          <Dialog 
-            open={isDialogOpen} 
-            onClose={closeCard} 
-            maxWidth="sm" 
-            fullWidth
-          >
-            <DialogTitle>{selectedCard.title}</DialogTitle>
-            <DialogContent>
-              <ReactMarkdown
-                components={{
-                  img: ({ node, ...props }) => (
-                    <img
-                      {...props}
-                      style={{ 
-                        width: "100%", 
-                        height: "auto", 
-                        cursor: "pointer" 
-                      }}
-                      alt={props.alt || "Image"}
-                      onClick={() => openImage(props.src || "")}
-                    />
-                  ),
-                }}
-              >
-                {selectedCard.description}
-              </ReactMarkdown>
-              <Box>
-                <Typography variant="subtitle1">{strings.cardScreen.comments}:</Typography>
-                {selectedCard?.comments?.map((comment: any, index: any) => (
-                  <Typography 
-                    key={index} 
-                    variant="body2" 
-                    sx={{ pl: 2 }}
-                  >
-                    - {comment.text || `${strings.cardRequestError.noText}`} ({members.find(member => member.memberId === comment.createdBy)?.fullName || `${strings.cardRequestError.unknownAuthor}`} -{" "}
-                    {members.find(member => member.memberId === comment.createdBy)?.email || `${strings.cardRequestError.noEmail}`})
-                  </Typography>
-                ))}
-              </Box>
-              <TextField
-                label={strings.cardScreen.addComment}
-                value={comment}
-                onChange={(event) => setComment(event.target.value)}
-                fullWidth
-                sx={{ mt: 1 }}
-              />
-              <Button onClick={createComment} variant="contained" sx={{ mt: 1 }}>
-                {strings.cardScreen.addComment}
-              </Button>
-            </DialogContent>
-          </Dialog>
+          <CardDialog
+            isOpen={isDialogOpen}
+            selectedCard={selectedCard}
+            members={members}
+            comment={comment}
+            onCommentChange={(event) => setComment(event.target.value)}
+            onClose={closeCard}
+            onCreateComment={createComment}
+            onImageClick={openImage}
+          />
         )}
-        <Dialog open={isImageOpen} onClose={closeImage} maxWidth="lg" fullWidth>
-          <DialogContent>
-            <Box sx={{ 
-              display: "flex", 
-              justifyContent: "flex-end", 
-              alignItems: "flex-end" 
-            }}>
-              {selectedImage ? (
-                <img
-                  src={selectedImage}
-                  alt="Full Size"
-                  style={{ 
-                    width: "100%", 
-                    height: "auto", 
-                    objectFit: "contain", 
-                    marginTop: "16px" 
-                  }}
-                />
-              ) : (
-                <Typography>{strings.cardRequestError.noImage}</Typography>
-              )}
-            </Box>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={closeImage} color="primary">
-              {strings.cardScreen.close}
-            </Button>
-          </DialogActions>
-        </Dialog>
+        <ImagePreviewDialog
+          isOpen={isImageOpen}
+          selectedImage={selectedImage}
+          onClose={closeImage}
+        />
       </Box>
     </Box>
   );
--- src/components/screens/memo-screen.tsx
+++ src/components/screens/memo-screen.tsx
@@ -1,29 +1,13 @@
 import { useEffect, useState } from "react";
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Card,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemButton,
-  ListItemText,
-  Typography
-} from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useLambdasApi } from "src/hooks/use-api";
-import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
-import { useAtom, useSetAtom } from "jotai";
-import { languageAtom } from "src/atoms/language";
+import { Box, Typography } from "@mui/material";
 import { DateTime } from "luxon";
-import dayjs from "dayjs";
-import { AutoAwesomeSharp, GTranslateSharp, PictureAsPdfSharp }  from "@mui/icons-material";
+import { useLambdasApi } from "src/hooks/use-api";
+import { useSetAtom } from "jotai";
 import strings from "src/localization/strings";
 import { PdfFile } from "src/generated/homeLambdasClient";
 import { errorAtom } from "src/atoms/error";
+import { PdfViewer } from "../memos/memo-viewer";
+import MemoList from "../memos/memo-list";
 
 /**
  * Memo screen component
@@ -51,7 +35,7 @@ const MemoScreen = () => {
       const validFiles = await memoApi.getMemos({ date: formattedDate });
       setFileList(validFiles);
       setSelectedFileId(validFiles[0]?.id);
-    } catch(error) {
+    } catch (error) {
       setError(`${strings.memoRequestError.fetchFileError}, ${error}`);
     }
   };
@@ -63,46 +47,17 @@ const MemoScreen = () => {
       width="100%" 
       height="50vh"
     >
-      <Card sx={{ 
-        width: "300px", 
-        p: 2, 
-        mr: 2 
-      }}>
-        <Typography 
-          variant="h6" 
-          mb={2} 
-          pl={"50px"}
-        >
-          {strings.memoScreen.selectPdf}
-        </Typography>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            label={strings.memoScreen.selectYear}
-            value={selectedYear ? dayjs(selectedYear.toISO()) : null}
-            onChange={(date) => {
-              if (date) {
-                setSelectedYear(DateTime.fromISO(date.toISOString()));
-              }
-            }}
-            views={["year"]}
-          />
-        </LocalizationProvider>
-        <List sx={{ mt: 2 }}>
-          {fileList.map((file) => (
-            <ListItem key={file.id} disablePadding>
-              <ListItemButton onClick={() => setSelectedFileId(file.id)}>
-                <ListItemIcon><PictureAsPdfIcon /></ListItemIcon>
-                <ListItemText primary={file.name} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Card>
-      <Box 
-        display="flex" 
-        flexDirection="column" 
-        flexGrow={1} 
-        alignItems="center" 
+      <MemoList
+        selectedYear={selectedYear}
+        setSelectedYear={setSelectedYear}
+        fileList={fileList}
+        setSelectedFileId={setSelectedFileId}
+      />
+      <Box
+        display="flex"
+        flexDirection="column"
+        flexGrow={1}
+        alignItems="center"
         justifyContent="center"
       >
         {selectedFileId ? (
@@ -114,204 +69,5 @@ const MemoScreen = () => {
     </Box>
   );
 };
-
-/**
- * Component to display a selected PDF file with services provided
- */
-const PdfViewer = ({ fileId }: { fileId: string }) => {
-  const [pdfBlobUrl, setPdfBlobUrl] = useState<string>();
-  const [loading, setLoading] = useState(true);
-  const [isTranslating, setIsTranslating] = useState(false);
-  const [translated, setTranslated] = useState(false);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [summaryText, setSummaryText] = useState<string>();
-  const [language] = useAtom(languageAtom);
-  const { memoApi } = useLambdasApi();
-  const setError = useSetAtom(errorAtom);
-
-  useEffect(() => {
-    if (fileId) fetchPdf();
-  }, [fileId]);
-
-  /**
-   * Fetches memo pdf file
-   */
-  const fetchPdf = async () => {
-    setLoading(true);
-    try {
-      if (pdfBlobUrl) URL.revokeObjectURL(pdfBlobUrl);
-      const pdfBlob = await memoApi.getContentMemo({ fileId });
-      const pdfUrl = URL.createObjectURL(pdfBlob);
-      setPdfBlobUrl(pdfUrl);
-      setTranslated(false);
-    } catch (err) {
-      setError(strings.memoRequestError.fetchPdfError);
-    }
-    setLoading(false);
-  };
-
-  const handleTranslatedPdf = async () => {
-    if (!translated) 
-      await translatePdf();
-    else await fetchPdf();
-  };
-
-  /**
-   * Fetches translated memo pdf file
-   */
-  const translatePdf = async () => {
-    setIsTranslating(true);
-    try {
-      const translatedPdf = await memoApi.getTranslatedMemoPdf({ fileId });
-      const pdfUrl = URL.createObjectURL(translatedPdf);
-      setPdfBlobUrl(pdfUrl);
-      setTranslated(true);
-    } catch (error) {
-      setError(strings.memoRequestError.downloadTranslatedError);
-    } 
-    setIsTranslating(false);
-  } 
-
-  /**
-   * Fetches summary for a memo
-   */
-  const handleSummary = async () => {
-    try {
-      setIsDialogOpen(true);
-      const summary = await memoApi.getSummaryMemo({ fileId });
-      const text = language === "fi" ? summary.fi ?? "" : summary.en ?? "";
-      setSummaryText(text);
-    } catch (error) {
-      setError(strings.memoRequestError.fetchSummaryError);
-    }
-  };
-
-  return (
-    <Card sx={{ 
-      p: 2, 
-      width: "100%", 
-      height: "100%",
-      position: "relative"
-    }}>
-      <Box 
-        display="flex" 
-        alignItems="center" 
-        gap={1}
-        mb={2}
-      >
-        <Box display="flex" alignItems="center" gap={0.5}>
-          <PictureAsPdfSharp />
-          <Button 
-            variant="text" 
-            href={pdfBlobUrl || ""} 
-            download 
-            disabled={isTranslating || !pdfBlobUrl}
-          >
-            {strings.memoScreen.download}
-          </Button>
-        </Box>
-        
-        <Box display="flex" alignItems="center" gap={0.5}>
-          <GTranslateSharp />
-          <Button onClick={handleTranslatedPdf} disabled={!fileId || isTranslating}>
-            {translated ? strings.memoScreen.originalPdf : strings.memoScreen.translatePdf}
-          </Button>
-        </Box>
-        
-        <Box display="flex" alignItems="center" gap={0.5}>
-          <AutoAwesomeSharp />
-          <Button onClick={handleSummary} disabled={isTranslating}>
-            {strings.memoScreen.viewSummary}
-          </Button>
-        </Box>
-      </Box>
-      {(loading || isTranslating) ? (
-        <Box sx={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "rgba(255, 255, 255, 0.7)",
-        }}
-        >
-          <CircularProgress />
-        </Box>
-      ) : (
-        pdfBlobUrl ? 
-          <PdfObject pdfBlobUrl={pdfBlobUrl} /> 
-        : 
-          <Typography variant="body1" color="error">
-            {strings.memoScreen.failedToLoadPdf}
-          </Typography>
-        )
-      }
-
-      {isDialogOpen && (
-        <Box sx={{
-          position: "absolute",
-          top: "20%",
-          left: "50%",
-          transform: "translate(-50%, -20%)",
-          bgcolor: "background.paper",
-          border: "1px solid #ccc",
-          boxShadow: 24,
-          p: 3,
-          width: "80%",
-        }}
-        >
-          <Typography variant="h6" gutterBottom>
-            {strings.memoScreen.summaryTitle}
-          </Typography>
-          {summaryText ? (
-            <Typography variant="body1">{summaryText}</Typography>
-          ) : (
-            <Box 
-              display="flex" 
-              justifyContent="center" 
-              alignItems="center" 
-              minHeight="100px"
-            >
-              <CircularProgress />
-            </Box>
-          )}
-          <Button onClick={() => setIsDialogOpen(false)} color="primary" fullWidth>
-            {strings.memoScreen.close}
-          </Button>
-        </Box>
-      )}
-    </Card>
-  );
-};
-
-/**
- * Displays the PDF in an object element for viewing within the browser
- */
-const PdfObject = ({pdfBlobUrl}: {pdfBlobUrl: string}) => {
-  const [bdfBlodUrl] = useState(pdfBlobUrl);
-  return (
-    <object
-      data={bdfBlodUrl + "#toolbar=0&zoom=90"}
-      type="application/pdf"
-      width="100%"
-      height="100%"
-      style = {{paddingBottom:"50px", backgroundColor:"white"}}
-    >
-      <Typography variant="body2" color="textSecondary">
-        {strings.memoScreen.unsupportedBrowser}{" "}
-        <Button 
-          variant="text" 
-          href={pdfBlobUrl} 
-          download
-        >
-          {strings.memoScreen.downloadPdf}
-        </Button>
-      </Typography>
-    </object>
-  )
-}
 
 export default MemoScreen;
